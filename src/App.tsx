b6e3d68153b734bfe15b027775fe13afe3753636
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2026-01-17 07:45:46 +0530

    Add Turnstile integration and update password functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import { Turnstile } from '@marsidev/react-turnstile'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [token, setToken] = useState(0)
-
+  const [token, setToken] = useState<string>("")
   return (
     <>
+      <input placeholder='OTP'></input>
+      <input placeholder='New password'></input>
 
+      <Turnstile onSuccess={(token) => {
+        setToken(token)
+      }} siteKey='0x4AAAAAAAXtEe2JIeAEUcjX' />
+      {/* use a valid siteKey of cloudflare */}
+
+      <button onClick={() => {
+        axios.post("http://localhost:3000/reset-password", {
+          email: "harkirat@gmail.com",
+          otp: "123456",
+          token: token,
+        })
+      }}>Update password</button>
     </>
   )
 }
